7497020cede12f8b08e83ff45fa3277c7153719b
Muritala David <davmuri1414@gmail.com>
2022-10-03 10:23:49 +0100

    update

--- dist/worker/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.keepInSync = void 0;
-const sync_1 = require("./sync");
-Object.defineProperty(exports, "keepInSync", { enumerable: true, get: function () { return sync_1.keepInSync; } });
