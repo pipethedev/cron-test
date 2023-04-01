9ae720230672067b1776d6ac1db8195cf35933e5
Abraham <abrahamugbeshe@gmail.com>
2023-04-01 17:34:06 +0100

    perf: move deployment task to core api

--- worker/sync.ts
+++ worker/sync.ts
@@ -1,10 +1,8 @@
-import { IDomain, IProject, Log, PROJECT_STATUS } from "@brimble/models";
+import { IDomain, IProject, PROJECT_STATUS } from "@brimble/models";
 import axios from "axios";
 import fs from "fs";
 import { Project } from "@brimble/models";
-import { spawn, exec } from "child_process";
-import path from "path";
-import { proxy, socket, useRabbitMQ } from "../config";
+import { proxy, useRabbitMQ } from "../config";
 import { QueueClass } from "../queue";
 import { Job, UnrecoverableError } from "bullmq";
 import { LeanDocument } from "mongoose";
@@ -19,18 +17,7 @@ const keepInSyncWorker = async (job: Job) => {
 
     if (!project) return;
 
-    const {
-      domains,
-      port,
-      dir,
-      outputDirectory,
-      buildCommand,
-      name,
-      rootDir,
-      _id,
-      log,
-      pid: oldPid,
-    } = project;
+    const { domains, port, dir, name, log } = project;
 
     const shouldStart = await starter({
       domains,
@@ -38,135 +25,20 @@ const keepInSyncWorker = async (job: Job) => {
       dir,
       name,
       log,
-      id: _id,
+      id,
     });
     if (!shouldStart) return;
-
-    let done = false,
-      failed = false;
-
-    console.log(`Redeploying ${name}...`);
-    const deployLog = `${dir}/deploy.log`;
-    const startTime = new Date().toISOString();
-
-    const fileDir = rootDir ? path.join(dir, rootDir) : dir;
-    const start = spawn(
-      "nohup",
-      [
-        "brimble",
-        "dev",
-        `${fileDir}`,
-        `${port ? `"-p ${port}"` : ""}`,
-        buildCommand ? "--build-command" : "",
-        buildCommand ? `"${buildCommand}"` : "",
-        outputDirectory ? "--output-directory" : "",
-        outputDirectory ? `"${outputDirectory}"` : "",
-        ">>",
-        deployLog,
-        "2>&1",
-        "&",
-      ],
-      { shell: true }
+    return useRabbitMQ(
+      "main",
+      "send",
+      JSON.stringify({
+        event: "redeploy",
+        data: { projectId: id, startOnly: true },
+      })
     );
-
-    start.stderr.on("data", (data) => {
-      const message = data.toString();
-      fs.createWriteStream(deployLog, message);
-    });
-
-    start.on("close", (code) => {
-      if (code !== 0) {
-        fs.createWriteStream(
-          deployLog,
-          `child process exited with code ${code as any}`
-        );
-      }
-    });
-
-    const watcher = spawn("tail", ["-f", deployLog]);
-    watcher.stdout.on("data", async (data: any) => {
-      const buff = data.toString();
-      const messages = buff.split("\n");
-
-      await Promise.all(
-        messages?.map(async (message: string) => {
-          message = message.trim().toLowerCase();
-          if (message.includes("failed")) {
-            const status = PROJECT_STATUS.FAILED;
-            await Project.findByIdAndUpdate(_id, { status });
-            log &&
-              (await Log.findByIdAndUpdate(log._id, {
-                status,
-                startTime,
-                endTime: new Date().toISOString(),
-              }));
-
-            failed = true;
-          }
-        })
-      );
-
-      const pid = buff.match(/PID: \d+/g);
-      const url = buff.match(/http:\/\/[a-zA-Z0-9-.]+:[0-9]+/g);
-      if (url && pid) {
-        let urlString = url[0];
-        const port = urlString.match(/:[0-9]+/g);
-
-        urlString = urlString.replace("localhost", "127.0.0.1");
-
-        await Project.findByIdAndUpdate(project?._id, {
-          pid: pid?.[0].split(":")[1].trim(),
-          port: port?.[0].split(":")[1].trim(),
-          status: PROJECT_STATUS.ACTIVE,
-        });
-        log &&
-          (await Log.findByIdAndUpdate(log._id, {
-            status: PROJECT_STATUS.ACTIVE,
-            startTime,
-            endTime: new Date().toISOString(),
-          }));
-
-        domains.forEach((domain: IDomain) => {
-          proxy.register(domain.name, urlString, {
-            isWatchMode: true,
-          });
-          socket.emit("domain:clear_cache", {
-            domain: domain.name,
-          });
-        });
-        done = true;
-      }
-    });
-
-    while (!done && !failed)
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    await new Promise((resolve) =>
-      setTimeout(() => {
-        exec(`kill -9 ${watcher.pid}`);
-        exec(`kill -9 ${start.pid}`);
-        if (done) {
-          console.log(`${name} redeployed 🚀`);
-          exec(`kill -9 ${oldPid}`);
-          exec(`pkill -f jest-worker/processChild.js`);
-        }
-        resolve(true);
-      }, 10000)
-    );
-
-    if (done) {
-      console.log(`Project ${project.name} deployed successfully`);
-      return true;
-    } else if (failed) {
-      console.log(`${project.name} deploy failed`);
-      throw new UnrecoverableError("Deploy failed");
-    } else {
-      console.log(`${project.name} deploy timed out`);
-      throw new UnrecoverableError("Deploy timed out");
-    }
   } catch (error: any) {
-    console.error(`Couldn't start | ${error.message}`);
-    throw new UnrecoverableError(`Couldn't start | ${error.message}`);
+    console.error(error.message);
+    throw new UnrecoverableError(error.message);
   }
 };
 
