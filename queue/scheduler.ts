f43dc25747c5dee41ac734d251f54646fa4daa12
Abraham <abrahamugbeshe@gmail.com>
2023-04-03 23:47:51 +0100

    added cron interval to env

--- queue/scheduler.ts
+++ queue/scheduler.ts
@@ -2,7 +2,9 @@ import cron from "node-cron";
 import { keepInSync } from "../worker/sync";
 
 const useScheduler = () => {
-  const syncScheduler = cron.schedule("*/5 * * * *", () => keepInSync());
+  const syncScheduler = cron.schedule(process.env.CRON || "*/5 * * * *", () =>
+    keepInSync()
+  );
 
   return [syncScheduler];
 };
