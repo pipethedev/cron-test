2b8afe3544d9fdab839c0b448da26800c0bd1596
Abraham <abrahamugbeshe@gmail.com>
2023-04-13 14:18:27 +0100

    perf: keeping track of processed projects

--- worker/sync.ts
+++ worker/sync.ts
@@ -52,7 +52,12 @@ export const projectSync = new QueueClass("project-sync", keepInSyncWorker);
 
 export const keepInSync = async () => {
   const projects = await Project.find();
+  const now = Date.now();
   const data = projects
+    .filter((project: LeanDocument<IProject>) => {
+      const timeElapsed = now - (project.lastProcessed || 0);
+      return timeElapsed > 30 * 60 * 1000; // only retry projects processed more than 30 minutes ago
+    })
     .sort((a, b) => {
       const aIndex = prioritize.indexOf(a.name);
       const bIndex = prioritize.indexOf(b.name);
@@ -68,6 +73,8 @@ export const keepInSync = async () => {
       return Number(b.createdAt) - Number(a.createdAt);
     })
     .map((project: LeanDocument<IProject>) => ({ data: { id: project._id } }));
+
+  await Promise.all(data.map(({ data: { id } }) => Project.updateOne({ _id: id }, { lastProcessed: now })));
 
   await projectSync.executeBulk(data);
 };
